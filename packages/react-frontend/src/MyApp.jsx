5645e96c8715a88506adb2674aeb1a614adf67e4
Emmy <ameliaharris492@gmail.com>
2026-01-20 15:50:03 -0800

    fix: implemented it so front end delets only if backend sucessfully deletes

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -5,47 +5,58 @@ import Form from "./Form";
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-
-  
   function removeOneCharacter(index) {
-    const updated = characters.filter((characters, i) => {
-      return i !== index;
-    });
-    setCharacters(updated);
-    const promise = fetch(`http://localhost:8000/users/${characters[index].id}`,{
-      method: 'DELETE'
-    });
-  }
-
-  function fetchUsers(){
-    const promise = fetch('http://localhost:8000/users');
-    return (promise);
-  }
-
-  function postUser(person){
-    
-    const promise = fetch('http://localhost:8000/users',{
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
+    const promise = fetch(
+      `http://localhost:8000/users/${characters[index].id}`,
+      {
+        method: "DELETE",
       },
-      body: JSON.stringify(person)
+    );
+    promise.then((res) => {
+      if (res.status === 204) {
+        const updated = characters.filter((characters, i) => {
+          return i !== index;
+        });
+        setCharacters(updated);
+      }
+      else{
+        console.error("Error deleting user:", res.statusText);
+      }
     });
-    return (promise);
   }
 
-  function updateList(person){
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+    return promise;
+  }
+
+  function updateList(person) {
     postUser(person)
-      .then((res) => res.status === 201? res.json(): undefined)
-      .then((json) =>{if(json){setCharacters([...characters, json])}})
-      .catch((error) => console.error('Error posting user:', error));
+      .then((res) => (res.status === 201 ? res.json() : undefined))
+      .then((json) => {
+        if (json) {
+          setCharacters([...characters, json]);
+        }
+      })
+      .catch((error) => console.error("Error posting user:", error));
   }
 
   useEffect(() => {
     fetchUsers()
       .then((res) => res.json())
       .then((json) => setCharacters(json.users_list))
-      .catch((error) => console.error('Error fetching users:', error));
+      .catch((error) => console.error("Error fetching users:", error));
   }, []);
 
   return (
